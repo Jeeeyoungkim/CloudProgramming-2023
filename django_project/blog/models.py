8787d6ba78fb7ed4c30b0397caeba64a7faf5a66
Jeeeyoungkim <102647933+Jeeeyoungkim@users.noreply.github.com>
2023-03-21 17:24:01 +0900

    상세보기 기능 추가

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -4,9 +4,12 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_ad = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     #Post 모델의 타이틀이 대표가 됨
     def __str__(self): #string 내장함수 오버라이딩
         return f'[{self.pk}] {self.title}'
+
+    def get_absolute_url(self):
+        return f'/blog/{self.pk}/'
 
