d8b2c485b1dd49f6ba394ad8d8ad985efd25e00f
Jeeeyoungkim <102647933+Jeeeyoungkim@users.noreply.github.com>
2023-03-21 15:19:58 +0900

    Edit blog model

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_ad = models.DateTimeField(auto_now=True)
 
     #Post 모델의 타이틀이 대표가 됨
     def __str__(self): #string 내장함수 오버라이딩
