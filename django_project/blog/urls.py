c975ee3a6c4a34e7f4dd466d97607dab0837e717
Jeeeyoungkim <102647933+Jeeeyoungkim@users.noreply.github.com>
2023-03-21 16:48:42 +0900

    create view post

---
+++ django_project/blog/urls.py
@@ -0,0 +1,7 @@
+# urlconfig for blog
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+# index의 매개변수는 request(요청) or response(대답) ? 외워?
+def index(request):
+    # 사용자의 요청을 받아서 이런일 저런일을 ...
+    posts = Post.objects.all().order_by('-pk')
+
+    return render(
+        request, 'blog/index.html', {'posts': posts})
