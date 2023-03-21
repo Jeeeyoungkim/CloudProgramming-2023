5feb6584b3a645b26b0851bf63c1f10b2518d0fe
Jeeeyoungkim <102647933+Jeeeyoungkim@users.noreply.github.com>
2023-03-21 17:32:49 +0900

    create main page

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('blog/', include('blog.urls')), # blog한테 떠넘기기 !?
     path('admin/', admin.site.urls),
+    path('', include('single_pages.urls'))
 ]
