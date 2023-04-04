bdb29366d81d7891cbbf7ab11430022b49cd2584
Jeeeyoungkim <102647933+Jeeeyoungkim@users.noreply.github.com>
2023-04-04 16:17:43 +0900

    add Category model

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+import os
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
